370da29df1d34cd9e16a0ae1486f747a237251be
Cristian R <cristianrom66@gmail.com>
2022-05-03 01:32:58 -0500

    Updates

---
+++ precache-manifest.16b515aa1806829f12f03afcbacc34b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51db555a5fb2ecacb117321064b8de5",
+    "url": "/nombre-del-repositorio/index.html"
+  },
+  {
+    "revision": "e8b448920349b2ac7e43",
+    "url": "/nombre-del-repositorio/static/css/main.e5939a50.chunk.css"
+  },
+  {
+    "revision": "4ace20dfbc1fbe8c04c4",
+    "url": "/nombre-del-repositorio/static/js/2.dec8a867.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/nombre-del-repositorio/static/js/2.dec8a867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b448920349b2ac7e43",
+    "url": "/nombre-del-repositorio/static/js/main.6d60f551.chunk.js"
+  },
+  {
+    "revision": "74c353e7638aac5e2581",
+    "url": "/nombre-del-repositorio/static/js/runtime-main.2353f6b3.js"
+  },
+  {
+    "revision": "715313cd4f8b7b826663309b8751bb6b",
+    "url": "/nombre-del-repositorio/static/media/svg-1.715313cd.svg"
+  },
+  {
+    "revision": "4a122f73bc839268339ab652e3ec374d",
+    "url": "/nombre-del-repositorio/static/media/svg-2.4a122f73.svg"
+  },
+  {
+    "revision": "d9e440a5ed9283a035d51b4cb93663f0",
+    "url": "/nombre-del-repositorio/static/media/svg-3.d9e440a5.svg"
+  },
+  {
+    "revision": "72ed571599b33a2d2810daa85d57e890",
+    "url": "/nombre-del-repositorio/static/media/video.72ed5715.mp4"
+  }
+]);
